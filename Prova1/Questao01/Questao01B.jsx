28b9354dec7708075694bfcf685602d1a9a8b813
gimoncayo <91566104+gimoncayo@users.noreply.github.com>
2023-05-15 10:50:31 -0300

    Update Questao01B.jsx

--- Prova1/Questao01/Questao01B.jsx
+++ Prova1/Questao01/Questao01B.jsx
@@ -5,6 +5,7 @@ function Questao01B({alunos, sendMedia}) {
     useEffect(() => {
         const medias = alunos.map(aluno => (aluno.notas.ap1 + aluno.notas.ap2) / 2)
         sendMedia(medias)
+        //chama a função map na lista de alunos para calcular a média de cada aluno e enviar as médias calculadas
       }
       , 
       [alunos, sendMedia]
