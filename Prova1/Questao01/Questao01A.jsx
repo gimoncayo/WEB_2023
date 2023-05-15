e5354fcfe31265ac5b76654ebe85152ab7ac7601
gimoncayo <91566104+gimoncayo@users.noreply.github.com>
2023-05-15 10:48:12 -0300

    Update Questao01A.jsx

--- Prova1/Questao01/Questao01A.jsx
+++ Prova1/Questao01/Questao01A.jsx
@@ -2,7 +2,8 @@ import {useState} from "react"
 import Questao01B from "./Questao01B"
 
 const Questao01A = () => {
-
+    
+    //declara uma variável de estado medias e permite atualizar o valor do estado
     const [medias, setMedias] = useState(0)
 
     const alunos = [
@@ -10,11 +11,13 @@ const Questao01A = () => {
         { nome: "Beltrano", notas: {ap1: 6.7, ap2: 3.5} },
         { nome: "Sicrano", notas: {ap1: 7.3, ap2: 9.2} }
     ]
-
+    
+    //atualiza o estado medias com um novo valor recebido como parâmetro
     const enviarMedias = (NewMedias) => {
         setMedias(NewMedias);
       };
-
+    
+    //filtragem para imprimir apenas os alunos aprovados
     const alunosAprovados = alunos.filter((aluno, index) => medias[index] >= 6.0);
 
     return (
@@ -23,6 +26,7 @@ const Questao01A = () => {
           <Questao01B alunos={alunos} sendMedia = {enviarMedias}/>
           <h2>Alunos Aprovados:</h2>
           <>
+              //renderização da lista de alunos aprovados
             {alunosAprovados.map((aluno) => (
               <li key={aluno.nome}>{aluno.nome}</li>
             ))}
